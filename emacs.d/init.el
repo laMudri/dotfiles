bc541d4cb6d6cd311490c8be9fcae8b73cf55694
laMudri <lamudri@gmail.com>
2014-11-12 09:00:26 +0000

    overhaul of emacs.d

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,30 +1,25 @@
-(add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/elpa")
+
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
 
+(require 'linum-relative)
 (global-linum-mode)
 
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-f 't)
 (setq scroll-s 1)
-
-(add-to-list 'load-path "~/.emacs.d/lojban")
-(autoload 'lojban-parse-region "lojban" nil t)
-(autoload 'lojban-mode "lojban-mode" nil t)
-
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
 
 (add-to-list 'load-path "~/.emacs.d/j-mode/")
 (autoload 'j-mode "j-mode.el" "Major mode for editing J files" t)
@@ -41,7 +36,6 @@
 (require 'evil-leader)
 (global-evil-leader-mode)
 
-(add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
 
@@ -69,10 +63,18 @@
 (define-key evil-normal-state-map (kbd "M-x") 'execute-extended-command)
 (define-key evil-motion-state-map "|" 'evil-execute-in-emacs-state)
 
-(require 'package)
-(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+(require 'evil-escape)
+(setq evil-escape-key-sequence (kbd "C-c C-c"))
+(require 'evil-surround)
+(global-evil-surround-mode 1)
+(require 'evil-exchange)
+(setq evil-exchange-key (kbd "zx"))
+(setq evil-exchange-cancel-key (kbd "zx"))
+(evil-exchange-install)
 
-;(set-face-attribute 'default nil :height 100)
+(unless (display-graphic-p)
+  (require 'evil-terminal-cursor-changer))
+
 (setq initial-frame-alist '(
   (font . "Monospace-9")
 ))
@@ -93,3 +95,24 @@
 ;(sp-pair "<" ">" :wrap "C-s >")
 ;(sp-pair "\"" "\"" :wrap "C-s \"")
 ;(sp-pair "'" "'" :wrap "C-s '")
+
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
+(define-key evil-normal-state-map (kbd "S-SPC") 'ace-jump-mode)
+(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-char-mode)
+
+(autoload 'lojban-parse-region "lojban" nil t)
+(autoload 'lojban-mode "lojban-mode" nil t)
