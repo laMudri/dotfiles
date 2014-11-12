a00a5a93eadeef480c8bac0437993f05911e1b57
laMudri <lamudri@gmail.com>
2014-11-12 09:34:10 +0000

    cleaning up some weird artefacts

---
+++ emacs.d/init.el
@@ -0,0 +1,118 @@
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/elpa")
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+
+(require 'linum-relative)
+(global-linum-mode)
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-f 't)
+(setq scroll-s 1)
+
+(add-to-list 'load-path "~/.emacs.d/j-mode/")
+(autoload 'j-mode "j-mode.el" "Major mode for editing J files" t)
+(add-to-list 'auto-mode-alist '("\\.ij[rstp]$" . j-mode))
+
+;(custom-set-face
+; '(j-verb-face ((t (:foreground "cornflower blue"))))
+; '(j-adverb-face ((t (:foreground "goldenrod"))))
+; '(j-conjunction-face ((t (:foreground "olive drab"))))
+; '(j-other-face ((t (:foreground "Black")))))
+
+(require 'evil-indent-textobject)
+
+(require 'evil-leader)
+(global-evil-leader-mode)
+
+(require 'evil)
+(evil-mode 1)
+
+(setq evil-search-module 'evil-search)
+(setq evil-magic 'very-magic)
+
+(define-key evil-motion-state-map "," 'evil-ex)
+(define-key evil-motion-state-map ":" 'evil-repeat-find-char-reverse)
+
+(define-key evil-motion-state-map (kbd "C-n") 'evil-scroll-line-up)
+(define-key evil-normal-state-map (kbd "C-n") 'evil-scroll-line-up)
+
+(define-key evil-window-map "j" 'evil-window-left)
+(define-key evil-window-map "J" 'evil-window-move-far-left)
+(define-key evil-window-map "k" 'evil-window-down)
+(define-key evil-window-map "K" 'evil-window-move-very-bottom)
+(define-key evil-window-map "h" 'evil-window-up)
+(define-key evil-window-map "H" 'evil-window-move-very-top)
+
+(define-key evil-motion-state-map "j" 'evil-backward-char)
+(define-key evil-motion-state-map "J" 'evil-window-top)
+(define-key evil-motion-state-map "k" 'evil-next-line)
+(define-key evil-motion-state-map "h" 'evil-previous-line)
+
+(define-key evil-normal-state-map (kbd "M-x") 'execute-extended-command)
+(define-key evil-motion-state-map "|" 'evil-execute-in-emacs-state)
+
+(require 'evil-escape)
+(setq evil-escape-key-sequence (kbd "C-c C-c"))
+(require 'evil-surround)
+(global-evil-surround-mode 1)
+(require 'evil-exchange)
+(setq evil-exchange-key (kbd "zx"))
+(setq evil-exchange-cancel-key (kbd "zx"))
+(evil-exchange-install)
+
+(unless (display-graphic-p)
+  (require 'evil-terminal-cursor-changer))
+
+(setq initial-frame-alist '(
+  (font . "Monospace-9")
+))
+(setq default-frame-alist '(
+  (font . "Monospace-9")
+))
+
+(setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
+
+; AUCTeX
+(define-key evil-normal-state-map "gpp" 'preview-at-point)
+
+(smartparens-global-mode)
+(require 'smartparens-config)
+;(sp-pair "(" ")" :wrap "C-s )")
+;(sp-pair "[" "]" :wrap "C-s ]")
+;(sp-pair "{" "}" :wrap "C-s }")
+;(sp-pair "<" ">" :wrap "C-s >")
+;(sp-pair "\"" "\"" :wrap "C-s \"")
+;(sp-pair "'" "'" :wrap "C-s '")
+
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
+(define-key evil-normal-state-map (kbd "S-SPC") 'ace-jump-mode)
+(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-char-mode)
+
+(autoload 'lojban-parse-region "lojban" nil t)
+(autoload 'lojban-mode "lojban-mode" nil t)
