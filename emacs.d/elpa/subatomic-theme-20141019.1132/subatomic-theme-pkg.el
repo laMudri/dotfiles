99b7bbdfd7f3cedc9c27b8744145731ccfb61a4d
laMudri <lamudri@gmail.com>
2014-11-23 21:18:17 +0000

    I forgot to do commits again

---
+++ emacs.d/elpa/subatomic-theme-20141019.1132/subatomic-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "subatomic-theme" "20141019.1132" "Low contrast bluish color theme" 'nil :url "https://github.com/cryon/subatomic" :keywords '("color-theme" "blue" "low contrast"))
---
+++ emacs.d/elpa/zonokai-theme-20140909.1941/zonokai-theme-pkg.el
@@ -0,0 +1,4 @@
+(define-package "zonokai-theme" "20140909.1941" "No description available." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
