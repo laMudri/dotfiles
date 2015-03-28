c69a318da317d8ea72d93c10c5feddd995903c1a
laMudri <lamudri@gmail.com>
2015-03-28 10:37:10 +0000

    tweaking zsh

---
+++ emacs.d/elpa/solarized-theme-20150319.102/solarized-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; solarized-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "solarized" "solarized.el" (21776 44301 199719
+;;;;;;  750000))
+;;; Generated autoloads from solarized.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("solarized-dark-theme.el" "solarized-light-theme.el"
+;;;;;;  "solarized-theme-pkg.el" "solarized-theme-utils.el" "solarized-theme.el")
+;;;;;;  (21776 44302 147769 145000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; solarized-theme-autoloads.el ends here
