4c68c643e20821b45ab5a904e3cba949b76c436f
laMudri <lamudri@gmail.com>
2015-10-27 14:27:25 +0000

    added nvim things

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim gvimrc pentadactyl pentadactylrc emacs.d zshrc xkb"    # list of files/folders to symlink in homedir
+files="vimrc vim nvimrc nvim gvimrc pentadactyl pentadactylrc emacs.d zshrc xkb"    # list of files/folders to symlink in homedir
 
 ##########
 
