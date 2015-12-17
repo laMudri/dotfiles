ce033174d6a870bce6f96aea8ef4bcabc296cdc9
laMudri <lamudri@gmail.com>
2015-12-17 01:05:10 +0000

    removed elpa packages

--- emacs.d/elpa/markup-faces-20141110.17/markup-faces-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "markup-faces" "20141110.17" "collection of faces for markup language modes" 'nil :url "https://github.com/sensorflo/markup-faces" :keywords '("wp" "faces"))
--- emacs.d/elpa/prop-menu-20150728.418/prop-menu-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; prop-menu-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("prop-menu.el") (21984 59374 341991 443000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; prop-menu-autoloads.el ends here
